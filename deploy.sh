8206729f984855dbcfabc90b5fe4aff9a66d7035
Tej-Birring <tejbirring@gmail.com>
2022-06-04 22:56:26 +0100

    Improve Heroku deployment script.

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ set -e
 
 # Run cmd: push image to registry
 # (Builds the image from the Dockerfile and pushes the image to Heroku Container Registry for this Heroku project.)
-heroku container:push web
+heroku container:push web --app graph-based-server
 
 # Run cmd: release image to app
 # (Makes the image 'go live' by initiating a container instance for this Heroku project.)
 # See project settings in `dashboard.heroku.com` to set environment variables (called "Config Vars").
-heroku container:release web
+heroku container:release web --app graph-based-server
